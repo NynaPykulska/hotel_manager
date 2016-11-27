af9f8cb93d02548e533b287aa7116cfae35fd5d1
Nina Pikulska <Nina Pikulska>
2016-11-27 15:23:02 +0100

    init

---
+++ app/helpers/aside_helper.rb
@@ -0,0 +1,2 @@
+module AsideHelper
+end
---
+++ test/controllers/aside_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AsideControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
