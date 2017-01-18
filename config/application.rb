1e00173156111bc94cc98a83eb46d3f407260b44
Nina Pikulska <Nina Pikulska>
2017-01-18 22:32:51 +0100

    remove assets from version control

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module HotelManager
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.enabled = true
+    config.assets.digest = true
   end
 end
