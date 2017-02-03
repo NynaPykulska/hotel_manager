06be50f535f7f43efd5a56781e241cd36aff7306
Nina Pikulska <Nina Pikulska>
2017-02-04 00:05:59 +0100

    prod config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  Paperclip.options[:command_path] = "/home/micc/Pulpit/hotel_manager/app/assets/images"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
