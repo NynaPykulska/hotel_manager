ef910983bb3a1aed82c9454863bd89f9c6940376
Nina Pikulska <Nina Pikulska>
2017-03-07 22:42:57 +0100

    rooms

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -14,6 +14,8 @@ class RoomController < ApplicationController
 
   def list
        @room = Room.new
+       @issueTypes = IssueType.all
+
 
        if params[:group] == "all"
           @rooms = Room.all
@@ -31,6 +33,11 @@ class RoomController < ApplicationController
  	def init_items_size_list
     @items_size_list ||= [Room.all.size, Room.where("is_clean = ?", false).size, Room.where("is_clean = ?", true).size]
     #                             0                           1                                       2
-	end		
+	end	
+
+  def report_issue
+    puts "report"
+  end
+    	
 
 end
