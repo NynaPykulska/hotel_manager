9c42b0cae44a3c34a5cd8610ca2702ec59f4c709
Nina Pikulska <Nina Pikulska>
2017-03-09 21:08:30 +0100

    datepicker styles

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -58,13 +58,14 @@ class IssuesController < ApplicationController
 		@room = Room.where("room_id = ?", issue_params[:room_id]).first
 		
 		if(issue_params[:is_recurring] == "0")
-        @date = DateTime.new(params[:issue]["requested_fix_date(1i)"].to_i, params[:issue]["requested_fix_date(2i)"].to_i ,params[:issue]["requested_fix_date(3i)"].to_i)
+        
+        @date = Date.strptime(params[:issue]["requested_fix_date"], '%Y-%m-%d')
         @issue = @room.issues.create(room_id: issue_params[:room_id], fix_comment: issue_params[:fix_comment], requested_fix_date: @date, completion_date: nil, is_done: issue_params[:is_done], timestamp: issue_params[:timestamp], issue_type_id: issue_params[:issue_type], priority: priority_list[issue_params[:priority].to_i - 1])
         redirect_back(fallback_location: root_path)
 
     else
-      start_date =  DateTime.new(params[:issue]["start_date(1i)"].to_i, params[:issue]["start_date(2i)"].to_i ,params[:issue]["start_date(3i)"].to_i)
-      end_date =    DateTime.new(params[:issue]["end_date(1i)"].to_i, params[:issue]["end_date(2i)"].to_i ,params[:issue]["end_date(3i)"].to_i)
+      start_date = DateTime.strptime(params[:issue]["start_date"], '%Y-%m-%d')
+      end_date = DateTime.strptime(params[:issue]["end_date"], '%Y-%m-%d')
 
       # Generate a unique ID for an event
       id = DateTime.now.strftime("%Y%m%d%k%M%S%L")
--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -58,71 +58,71 @@ class MemosController < ApplicationController
 
    	end
 
- 	  def create
-      @room = Room.where("room_id = ?", memo_params[:room_id]).first
+ 	def create
+    @room = Room.where("room_id = ?", memo_params[:room_id]).first
 
-      if(memo_params[:is_recurring] == "0")
-        @date = DateTime.new(params[:memo]["completion_date(1i)"].to_i, params[:memo]["completion_date(2i)"].to_i ,params[:memo]["completion_date(3i)"].to_i, params[:memo]["memo_time(4i)"].to_i, params[:memo]["memo_time(5i)"].to_i)
-        @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: @date, completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: false )
+    if(memo_params[:is_recurring] == "0")
+      @date = DateTime.new(params[:memo]["completion_date(1i)"].to_i, params[:memo]["completion_date(2i)"].to_i ,params[:memo]["completion_date(3i)"].to_i, params[:memo]["memo_time(4i)"].to_i, params[:memo]["memo_time(5i)"].to_i)
+      @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: @date, completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: false )
+      redirect_back(fallback_location: root_path)
+
+    else
+      start_date =  DateTime.new(params[:memo]["start_date(1i)"].to_i, params[:memo]["start_date(2i)"].to_i ,params[:memo]["start_date(3i)"].to_i, params[:memo]["memo_time(4i)"].to_i, params[:memo]["memo_time(5i)"].to_i)
+      end_date =    DateTime.new(params[:memo]["end_date(1i)"].to_i, params[:memo]["end_date(2i)"].to_i ,params[:memo]["end_date(3i)"].to_i, params[:memo]["memo_time(4i)"].to_i, params[:memo]["memo_time(5i)"].to_i)
+
+      # Generate a unique ID for an event
+      id = DateTime.now.strftime("%Y%m%d%k%M%S%L")
+      id = id.to_i.to_s(36)
+      id = id.to_i(36)
+
+      case memo_params[:recurrence]
+      when "1"
+        (start_date.to_i .. end_date.to_i).step(1.day) do |f|
+          puts Time.at(f)
+          @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: Time.at(f), completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: true, event_id: id )
+        end
         redirect_back(fallback_location: root_path)
-
-      else
-        start_date =  DateTime.new(params[:memo]["start_date(1i)"].to_i, params[:memo]["start_date(2i)"].to_i ,params[:memo]["start_date(3i)"].to_i, params[:memo]["memo_time(4i)"].to_i, params[:memo]["memo_time(5i)"].to_i)
-        end_date =    DateTime.new(params[:memo]["end_date(1i)"].to_i, params[:memo]["end_date(2i)"].to_i ,params[:memo]["end_date(3i)"].to_i, params[:memo]["memo_time(4i)"].to_i, params[:memo]["memo_time(5i)"].to_i)
-
-        # Generate a unique ID for an event
-        id = DateTime.now.strftime("%Y%m%d%k%M%S%L")
-        id = id.to_i.to_s(36)
-        id = id.to_i(36)
-
-        case memo_params[:recurrence]
-        when "1"
-          (start_date.to_i .. end_date.to_i).step(1.day) do |f|
-            puts Time.at(f)
-            @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: Time.at(f), completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: true, event_id: id )
+      when "2"
+        (start_date.to_i .. end_date.to_i).step(7.day) do |f|
+          puts Time.at(f)
+          @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: Time.at(f), completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: true, event_id: id )
+        end
+        redirect_back(fallback_location: root_path)
+      when "3"
+        requested_day = start_date.day
+        start_date = start_date.change(day: 1)
+        max_days = 0
+        while start_date < end_date
+          max_days = Time.days_in_month(start_date.month, start_date.year)
+          if(requested_day <= max_days)
+            @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: start_date.change(day: requested_day), completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: true, event_id: id )
+          else
+            @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: start_date.change(day: max_days), completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: true, event_id: id )
           end
-          redirect_back(fallback_location: root_path)
-        when "2"
-          (start_date.to_i .. end_date.to_i).step(7.day) do |f|
-            puts Time.at(f)
-            @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: Time.at(f), completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: true, event_id: id )
-          end
-          redirect_back(fallback_location: root_path)
-        when "3"
-          requested_day = start_date.day
-          start_date = start_date.change(day: 1)
-          max_days = 0
-          while start_date < end_date
-            max_days = Time.days_in_month(start_date.month, start_date.year)
-            if(requested_day <= max_days)
-              @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: start_date.change(day: requested_day), completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: true, event_id: id )
+          start_date = start_date + 1.month
+        end       
+        redirect_back(fallback_location: root_path)
+      when "4"
+        pattern = memo_params[:pattern].delete(' ').split(",").map(&:to_i)
+        
+        start_date = start_date.change(day: 1)
+        max_days = 0
+
+        while start_date < end_date
+          max_days = Time.days_in_month(start_date.month, start_date.year)
+          pattern.each do |day|
+            if(day <= max_days)
+              @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: start_date.change(day: day), completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: true, event_id: id )
             else
               @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: start_date.change(day: max_days), completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: true, event_id: id )
             end
-            start_date = start_date + 1.month
-          end       
-          redirect_back(fallback_location: root_path)
-        when "4"
-          pattern = memo_params[:pattern].delete(' ').split(",").map(&:to_i)
-          
-          start_date = start_date.change(day: 1)
-          max_days = 0
-
-          while start_date < end_date
-            max_days = Time.days_in_month(start_date.month, start_date.year)
-            pattern.each do |day|
-              if(day <= max_days)
-                @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: start_date.change(day: day), completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: true, event_id: id )
-              else
-                @memo = @room.memos.create(room_id: memo_params[:room_id], description: memo_params[:description], deadline: start_date.change(day: max_days), completion_date: memo_params[:completion_date], is_done: memo_params[:is_done], time_stamp: memo_params[:time_stamp], is_recurring: true, event_id: id )
-              end
-            end
-            start_date = start_date + 1.month
           end
-
-          redirect_back(fallback_location: root_path)
+          start_date = start_date + 1.month
         end
+
+        redirect_back(fallback_location: root_path)
       end
+    end
   end
    
 	def memo_params
