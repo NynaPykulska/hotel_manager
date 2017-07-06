2f357bc1fce29a386628f05b025a49874968bb54
Michał Michalczewski <michalczewski.michal@gmail.com>
2017-07-06 16:36:46 +0200

    [fix] Fix an ommitted variable name after last refactor

--- app/models/room.rb
+++ app/models/room.rb
@@ -6,5 +6,5 @@ class Room < ApplicationRecord
   validates_presence_of :room_id
   has_many :issues, dependent: :destroy
   has_many :memos, dependent: :destroy
-  attr_accessor :selectedIssues
+  attr_accessor :selected_issues
 end
