2034965d0ce0d8b895f7b03f3d1ef2552907bead
Michał Michalczewski <michalczewski.michal@gmail.com>
2017-07-08 01:01:00 +0200

    [refactor] Start with minor refactors of the controller

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -3,158 +3,178 @@
 # unresolved issues, report new issue in a chosen room,
 # mark them as resolved, re-open, delete and edit them.
 class IssuesController < ApplicationController
-	protect_from_forgery with: :null_session
-	before_action :authenticate_user!
+  protect_from_forgery with: :null_session
+  before_action :authenticate_user!
 
-	def create
-		@issue = Issue.new(issue_params)
-
-		if @room.save
-		  redirect_to controller: 'issue', action: 'list'
-		else
-		  render :action => 'new'
-		end
-	end
-
-	def list
-
-    @issues = Issue.where("is_done = false or completion_date = ?", Date.today)
-
+  def list
+    @issues = Issue.where('is_done = false or completion_date = ?', Date.today)
     @done = 0
     @not_done = 0
-
     @issues.each do |m|
-      m.is_done ? @done+=1 : @not_done+=1
+      m.is_done ? @done += 1 : @not_done += 1
     end
+  end
 
- 	end
+  def mark_ready
+    @issue = Issue.find(params[:id])
+    @issue.update_attribute(:is_done, true)
+    @issue.update_attribute(:completion_date, Date.today)
 
-	def mark_ready
-		@issue = Issue.find(params[:id])
-		@issue.update_attribute(:is_done, true)
-		@issue.update_attribute(:completion_date, Date.today)
-
-    @all_room_issues = Issue.where("room_id = ? AND is_done = ?", @issue.room_id, false)
-    if(@all_room_issues.count == 0)
-      @room = Room.where("room_id = ?", @issue.room_id).first
+    @all_room_issues = Issue.where('room_id = ? AND is_done = ?',
+                                   @issue.room_id, false)
+    if @all_room_issues.count.zero?
+      @room = Room.where('room_id = ?', @issue.room_id).first
       @room.update_attribute(:is_clean, true)
     end
+    render nothing: true
+  end
 
-		render :nothing => true
-	end
-
-	def reopen
-		@issue = Issue.find(params[:id])
-		@issue.update_attribute(:is_done, false)
-		@issue.update_attribute(:completion_date, Date.today)
-		
-    @room = Room.where("room_id = ?", @issue.room_id).first
+  def reopen
+    @issue = Issue.find(params[:id])
+    @issue.update_attribute(:is_done, false)
+    @issue.update_attribute(:completion_date, Date.today)
+    @room = Room.where('room_id = ?', @issue.room_id).first
     @room.update_attribute(:is_clean, false)
 
-		render :nothing => true
-	end
+    render nothing: true
+  end
 
-	def show
- 		
- 	end
+  def show; end
 
-	def issue_params
-		params.require(:issue).permit(:room_id, :issue_type_id, :requested_fix_date, :fix_comment, :timestamp, :completion_date, :issue_type, :is_recurring, :start_date, :end_date, :recurrence, :pattern, :priority)
-	end
+  def issue_params
+    params.require(:issue).permit(:room_id, :issue_type_id,
+                                  :requested_fix_date, :fix_comment,
+                                  :timestamp, :completion_date,
+                                  :issue_type, :is_recurring,
+                                  :start_date, :end_date,
+                                  :recurrence, :pattern, :priority)
+  end
 
-	def new
-    @images = Dir.glob("app/assets/images/icons/issue/*.svg")
+  def new
+    @images = Dir.glob('app/assets/images/icons/issue/*.svg')
     @chosen_image = @images[0]
-
-
   end
 
   def create_issue_type
     id = IssueType.maximum(:id).next
-    IssueType.create(id: id, issue_description: params[:issue_type][:description], default_priority: params[:issue_type][:priority], icon_path: params[:issue_type][:icon_path])
+    IssueType.create(id: id,
+                     issue_description: params[:issue_type][:description],
+                     default_priority: params[:issue_type][:priority],
+                     icon_path: params[:issue_type][:icon_path])
     redirect_back(fallback_location: root_path)
   end
 
-	def create
-		priority_list = ["Low", "Medium", "High"]
-		@room = Room.where("room_id = ?", issue_params[:room_id]).first
-		
-		if(issue_params[:is_recurring] == "0")
-        
-        @date = Date.strptime(params[:issue]["requested_fix_date"], '%Y-%m-%d')
-        @issue = @room.issues.create(room_id: issue_params[:room_id], fix_comment: issue_params[:fix_comment], requested_fix_date: @date, completion_date: nil, is_done: issue_params[:is_done], timestamp: issue_params[:timestamp], issue_type_id: issue_params[:issue_type], priority: priority_list[issue_params[:priority].to_i - 1])
-        redirect_back(fallback_location: root_path)
+  def create
+    priority_list = %w[Low Medium High]
+    @room = Room.where('room_id = ?', issue_params[:room_id]).first
 
+    if issue_params[:is_recurring] == '0'
+      @date = Date.strptime(params[:issue]['requested_fix_date'], '%Y-%m-%d')
+      @issue = @room.issues.create(room_id: issue_params[:room_id],
+                                   fix_comment: issue_params[:fix_comment],
+                                   requested_fix_date: @date,
+                                   completion_date: nil,
+                                   is_done: issue_params[:is_done],
+                                   timestamp: issue_params[:timestamp],
+                                   issue_type_id: issue_params[:issue_type],
+                                   priority: priority_list[issue_params[:priority].to_i - 1])
+      redirect_back(fallback_location: root_path)
     else
-      start_date = DateTime.strptime(params[:issue]["start_date"], '%Y-%m-%d')
-      end_date = DateTime.strptime(params[:issue]["end_date"], '%Y-%m-%d')
+      start_date = DateTime.strptime(params[:issue]['start_date'], '%Y-%m-%d')
+      end_date = DateTime.strptime(params[:issue]['end_date'], '%Y-%m-%d')
 
       # Generate a unique ID for an event
-      id = DateTime.now.strftime("%Y%m%d%k%M%S%L")
+      id = DateTime.now.strftime('%Y%m%d%k%M%S%L')
       id = id.to_i.to_s(36)
       id = id.to_i(36)
 
       case issue_params[:recurrence]
-      when "1"
-        (start_date.to_i .. end_date.to_i).step(1.day) do |f|
+      when '1'
+        (start_date.to_i..end_date.to_i).step(1.day) do |f|
           puts Time.at(f)
-        	@issue = @room.issues.create(room_id: issue_params[:room_id], fix_comment: issue_params[:fix_comment], requested_fix_date: Time.at(f), completion_date: nil, is_done: issue_params[:is_done], timestamp: issue_params[:timestamp], issue_type_id: issue_params[:issue_type], priority: priority_list[issue_params[:priority].to_i - 1])
+          @issue = @room.issues.create(room_id: issue_params[:room_id],
+                                       fix_comment: issue_params[:fix_comment],
+                                       requested_fix_date: Time.at(f),
+                                       completion_date: nil,
+                                       is_done: issue_params[:is_done],
+                                       timestamp: issue_params[:timestamp],
+                                       issue_type_id: issue_params[:issue_type],
+                                       priority: priority_list[issue_params[:priority].to_i - 1])
         end
         redirect_back(fallback_location: root_path)
-      when "2"
-        (start_date.to_i .. end_date.to_i).step(7.day) do |f|
+      when '2'
+        (start_date.to_i..end_date.to_i).step(7.day) do |f|
           puts Time.at(f)
-          @issue = @room.issues.create(room_id: issue_params[:room_id], fix_comment: issue_params[:fix_comment], requested_fix_date: Time.at(f), completion_date: nil, is_done: issue_params[:is_done], timestamp: issue_params[:timestamp], issue_type_id: issue_params[:issue_type], priority: priority_list[issue_params[:priority].to_i - 1])
+          @issue = @room.issues.create(room_id: issue_params[:room_id],
+                                       fix_comment: issue_params[:fix_comment],
+                                       requested_fix_date: Time.at(f),
+                                       completion_date: nil,
+                                       is_done: issue_params[:is_done],
+                                       timestamp: issue_params[:timestamp],
+                                       issue_type_id: issue_params[:issue_type],
+                                       priority: priority_list[issue_params[:priority].to_i - 1])
         end
         redirect_back(fallback_location: root_path)
-      when "3"
+      when '3'
         requested_day = start_date.day
         start_date = start_date.change(day: 1)
         max_days = 0
         while start_date < end_date
           max_days = Time.days_in_month(start_date.month, start_date.year)
-          if(requested_day <= max_days)
-            @issue = @room.issues.create(room_id: issue_params[:room_id], fix_comment: issue_params[:fix_comment], requested_fix_date: start_date.change(day: requested_day), completion_date: nil, is_done: issue_params[:is_done], timestamp: issue_params[:timestamp], issue_type_id: issue_params[:issue_type], priority: priority_list[issue_params[:priority].to_i - 1])
+          if requested_day <= max_days
+            @issue = @room.issues.create(room_id: issue_params[:room_id],
+                                         fix_comment: issue_params[:fix_comment],
+                                         requested_fix_date: start_date.change(day: requested_day),
+                                         completion_date: nil,
+                                         is_done: issue_params[:is_done],
+                                         timestamp: issue_params[:timestamp],
+                                         issue_type_id: issue_params[:issue_type],
+                                         priority: priority_list[issue_params[:priority].to_i - 1])
           else
-            @issue = @room.issues.create(room_id: issue_params[:room_id], fix_comment: issue_params[:fix_comment], requested_fix_date: start_date.change(day: max_days), completion_date: nil, is_done: issue_params[:is_done], timestamp: issue_params[:timestamp], issue_type_id: issue_params[:issue_type], priority: priority_list[issue_params[:priority].to_i - 1])
+            @issue = @room.issues.create(room_id: issue_params[:room_id],
+                                         fix_comment: issue_params[:fix_comment],
+                                         requested_fix_date: start_date.change(day: max_days),
+                                         completion_date: nil,
+                                         is_done: issue_params[:is_done],
+                                         timestamp: issue_params[:timestamp],
+                                         issue_type_id: issue_params[:issue_type],
+                                         priority: priority_list[issue_params[:priority].to_i - 1])
           end
-          start_date = start_date + 1.month
-        end       
+          start_date += 1.month
+        end
         redirect_back(fallback_location: root_path)
-      when "4"
-        pattern = issue_params[:pattern].delete(' ').split(",").map(&:to_i)
-        
+      when '4'
+        pattern = issue_params[:pattern].delete(' ').split(',').map(&:to_i)
         start_date = start_date.change(day: 1)
         max_days = 0
 
         while start_date < end_date
           max_days = Time.days_in_month(start_date.month, start_date.year)
           pattern.each do |day|
-            if(day <= max_days)
+            if day <= max_days
               @issue = @room.issues.create(room_id: issue_params[:room_id], fix_comment: issue_params[:fix_comment], requested_fix_date: start_date.change(day: day), completion_date: nil, is_done: issue_params[:is_done], timestamp: issue_params[:timestamp], issue_type_id: issue_params[:issue_type], priority: priority_list[issue_params[:priority].to_i - 1])
             else
               @issue = @room.issues.create(room_id: issue_params[:room_id], fix_comment: issue_params[:fix_comment], requested_fix_date: start_date.change(day: max_days), completion_date: nil, is_done: issue_params[:is_done], timestamp: issue_params[:timestamp], issue_type_id: issue_params[:issue_type], priority: priority_list[issue_params[:priority].to_i - 1])
             end
           end
-          start_date = start_date + 1.month
+          start_date += 1.month
         end
 
         redirect_back(fallback_location: root_path)
       end
     end
+  end
 
-	end
+  def delete
+    Issue.find(params[:id]).destroy
+    redirect_back(fallback_location: root_path)
+  end
 
-	def delete
- 		Issue.find(params[:id]).destroy
- 		redirect_back(fallback_location: root_path)
- 	end
-
- 	def edit
+  def edit
     @c = Issue.find(params[:id])
   end
-   
- 	def update
+
+  def update
     @issue = Issue.find(params[:id])
     @issue.update_attributes(issue_params)
     redirect_back(fallback_location: root_path)
