83e117e3506493b408d4bc81214bea999e840b00
Michał Michalczewski <michalczewski.michal@gmail.com>
2017-07-12 22:00:41 +0200

    [task] Add rendering error pages for record not found and standard error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render :template => "errors/record_not_found"
+  end
+
+  rescue_from StandardError do |e|
+    render :template => "errors/internal_error"
+  end
+
   def authenticate_user!(options = {})
     if user_signed_in?
       super(options)
